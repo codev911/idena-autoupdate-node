4d3b76045d5f7c908e293f0acd7eac91f41e62eb
Geraldine Adi <53482087+codev911@users.noreply.github.com>
2020-03-26 13:55:47 +0700

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -2,6 +2,12 @@
 while true
 do
 	check_ver () {
+		FILE=tmpver.txt
+		if [ ! -f "$FILE" ]; then
+			echo "$FILE does not exist, creating.."
+			touch tmpver.txt
+		fi
+
 		local ver_result=$(curl --silent "https://api.github.com/repos/idena-network/idena-go/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 		echo "$ver_result"
 	}
